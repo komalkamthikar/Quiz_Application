61c5e8efcd061344601c678c65bccc6785dcff5f
komalkamthikar <116787212+komalkamthikar@users.noreply.github.com>
2022-12-22 15:47:56 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,129 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+  
+    numb: 6,
+    question: "How do you create a function in JavaScript?",
+    answer: "function myFunction()",
+    options: [
+    "function myFunction()",
+    "function:myFunction()",
+    "function = myFunction()",
+    "None"
+     ]
+   },
+   {
+    numb: 7,
+    question: "How to write an IF statement in JavaScript?",
+    answer: "if (i == 5)",
+    options: [
+    "if i = 5 then",
+    "if i == 5 then",
+    "if (i == 5)",
+    " if i = 5",
+    ]
+   },
+   {
+    numb: 8,
+    question: "How can you add a comment in a JavaScript?",
+    answer: "//This is a comment",
+    options: [
+    "//This is a comment",
+    "/*This is a comment",
+    "#This is a comment",
+    "None"
+    ]
+   },
+   {
+    numb: 9,
+    question: "What is the correct way to write a JavaScript array?",
+    answer: "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
+    options: [
+    "var colors = (1:&quot;red&quot;, 2:&quot;green&quot;, 3:&quot;blue&quot;)",
+    "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
+    "var colors = 1 = (&quot;red&quot;), 2 = (&quot;green&quot;), 3 = (&quot;blue&quot;)",
+    "var colors = &quot;red&quot;, &quot;green&quot;, &quot;blue&quot;"
+    ]
+   },
+   {
+    numb: 10,
+    question: "How do you declare a JavaScript variable?",
+    answer: "var carName;",
+    options: [
+    "var carName;",
+    "variable carName;",
+    "v carName;",
+    "All of the above"
+    ]
+   }
+
+   // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+   
+];
